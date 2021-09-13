e7b7a1aab8542899f784a505229e3d98485c5b87
StefanVitor <vitorovic.stefan@gmail.com>
2021-09-13 15:19:11 +0200

    Fix network bridge part; Add bridge faucet API call;

---
+++ src/views/Bridge/utils/txUtils.ts
@@ -0,0 +1,31 @@
+
+
+export const formatValue = (num: any, dec: number) => {
+    const number = window.BigInt(num);
+    const round = window.BigInt(10 ** Number(dec));
+    const value = Number((number * window.BigInt(1000)) / round) / 1000;
+    return value.toFixed(4);
+};
+
+const countDecimals = (value: any) => {
+    if (Math.floor(value) === value) return 0;
+    return value.toString().split('.')[1].length || 0;
+};
+
+export const parseValue = (num: any, dec: number) => {
+    if (!num) {
+        return window.BigInt(0);
+    }
+    const number = Number(num);
+    const numberDec = countDecimals(number);
+    const round = window.BigInt(10 ** Number(dec));
+    const value =
+        (window.BigInt(Math.floor(number * 10 ** numberDec)) * round) /
+        window.BigInt(10 ** numberDec);
+    return value;
+};
+
+export const fetchGasPrice = async function(provider: any) {
+    const gasPrice = await provider.getGasPrice()
+    return gasPrice
+}
