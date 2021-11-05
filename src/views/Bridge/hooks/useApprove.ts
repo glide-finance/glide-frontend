3319bd64f839bc5d66987a245161e3efd05eba5f
Ryan <racollette@gmail.com>
2021-11-05 17:30:02 -0600

    fix: bridge contract approval

--- src/views/Bridge/hooks/useApprove.ts
+++ src/views/Bridge/hooks/useApprove.ts
@@ -12,9 +12,10 @@ import { useTranslation } from 'contexts/Localization'
 import useToast from 'hooks/useToast'
 import useLastUpdated from 'hooks/useLastUpdated'
 // import { parseValue, fetchGasPrice } from "../utils/txUtils";
-import { parseValue } from "../utils/txUtils";
+// import { parseValue } from "../utils/txUtils";
+import { foreignOrigin } from './useBridgeMediator';
 
-export const useCheckMediatorApprovalStatus = (currency, request, amount) => {
+export const useCheckMediatorApprovalStatus = (currency, request, amount, reverseBridgeParams) => {
   // const [isMediatorApproved, setIsMediatorApproved] = useState(false)
   const [needsApproval, setNeedsApproval] = useState(false)
   const { account, library } = useWeb3React()
@@ -30,9 +31,11 @@ export const useCheckMediatorApprovalStatus = (currency, request, amount) => {
     // const value = parseValue(amount, currency.decimals).toString()
     // const value = ethers.BigNumber.from(String(parseValue(amount, currency.decimals))).toString();
 
+    const mediator = foreignOrigin(currency.address, currency.chainId) ? reverseBridgeParams.contract : request.contract;
+
     const checkApprovalStatus = async () => {
       try {
-        const response = await tokenContract.allowance(account, request.contract)
+        const response = await tokenContract.allowance(account, mediator)
         const currentAllowance = new BigNumber(response.toString())
         // console.log(currentAllowance.toString())
         // console.log(value)
@@ -43,12 +46,12 @@ export const useCheckMediatorApprovalStatus = (currency, request, amount) => {
     }
 
     checkApprovalStatus()
-  }, [currency, request, library, account, lastUpdated, amount])
+  }, [currency, request, library, account, lastUpdated, amount, reverseBridgeParams])
 
   return needsApproval
 }
 
-export const useApproveMediator = (currency, request ) => {
+export const useApproveMediator = (currency, request, reverseBridgeParams) => {
   const [requestedApproval, setRequestedApproval] = useState(false)
   const [approvalComplete, setApprovalComplete] = useState(false)
   const { toastSuccess, toastError } = useToast()
@@ -60,13 +63,14 @@ export const useApproveMediator = (currency, request ) => {
     const isToken = currency instanceof Token
     if (!isToken) { setRequestedApproval(false); return }
     if (request === undefined) { setRequestedApproval(false); return }
+    const mediator = foreignOrigin(currency.address, currency.chainId) ? reverseBridgeParams.contract : request.contract;
     const tokenContract = getBep20Contract(currency.address, library.getSigner(account))
-    const response = (await tokenContract.allowance(account, request.contract))
+    const response = (await tokenContract.allowance(account, mediator))
     const allowance = ethersToBigNumber(response)
     try {
       if (!allowance.gt(0)) {
         setRequestedApproval(true)
-        const tx = await tokenContract.approve(request.contract, ethers.constants.MaxUint256)
+        const tx = await tokenContract.approve(mediator, ethers.constants.MaxUint256)
         const receipt = await tx.wait()
         // dispatch(updateUserAllowance(sousId, account))
         if (receipt.status) {
@@ -89,7 +93,7 @@ export const useApproveMediator = (currency, request ) => {
       console.error(e)
       toastError(t('Error'), t('Please try again. Confirm the transaction and make sure you are paying enough gas!'))
     }
-  }, [currency, account, library, request, t, toastError, toastSuccess])
+  }, [currency, account, library, request, t, toastError, toastSuccess, reverseBridgeParams])
 
   return { handleApprove, requestedApproval, approvalComplete }
 }
