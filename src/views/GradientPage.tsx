404ca2d2b9f7864e47b9190ffbe01ca57624942e
Ryan <racollette@gmail.com>
2021-11-10 22:03:33 -0700

    adjust pool display threshold

--- src/views/GradientPage.tsx
+++ src/views/GradientPage.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import styled from 'styled-components'
-import { Flex, Text } from '@glide-finance/uikit'
+import { Flex } from '@glide-finance/uikit'
 // import Footer from 'components/Menu/Footer'
 import SubNav from 'components/Menu/SubNav'
-import { useTranslation } from 'contexts/Localization'
+// import { useTranslation } from 'contexts/Localization'
 
 const StyledPage = styled.div`
   display: flex;
@@ -27,32 +27,32 @@ const StyledPage = styled.div`
     padding-top: 10vh;
     min-height: 100vh;
     background: radial-gradient(40% 55% at 45% 57.5%, #f2ad6c 0%, rgba(242, 173, 108, 0.4) 25%, rgba(6, 9, 20, 0) 72.5%),
-    radial-gradient(40% 45% at 55% 47.5%, #48b9ff 0%, rgba(72, 185, 255, 0.4) 25%, rgba(6, 9, 20, 0) 72.5%);
+      radial-gradient(40% 45% at 55% 47.5%, #48b9ff 0%, rgba(72, 185, 255, 0.4) 25%, rgba(6, 9, 20, 0) 72.5%);
     filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
     background-position-y: -8vh;
   }
 `
-const LaunchWarning = styled(Flex)`
-  border: 1px solid ${({ theme }) => theme.colors.primary};
-  border-radius: 16px;
-  margin-bottom: 20px;
-  padding: 16px;
-  max-width: 436px;
-  width: 100%;
-`
+// const LaunchWarning = styled(Flex)`
+//   border: 1px solid ${({ theme }) => theme.colors.primary};
+//   border-radius: 16px;
+//   margin-bottom: 20px;
+//   padding: 16px;
+//   max-width: 436px;
+//   width: 100%;
+// `
 
 const GradientPage: React.FC<React.HTMLAttributes<HTMLDivElement>> = ({ children, ...props }) => {
-  const { t } = useTranslation()
+  // const { t } = useTranslation()
 
   return (
     <StyledPage {...props}>
-      <LaunchWarning>
+      {/* <LaunchWarning>
         <Text bold color="primary">
           {t(
             'The GLIDE supply is limited due to fair launch! Volatility will be high for a few days. Play safe!',
           )}
         </Text>
-      </LaunchWarning>
+      </LaunchWarning> */}
       <SubNav />
       {children}
       <Flex flexGrow={1} />
