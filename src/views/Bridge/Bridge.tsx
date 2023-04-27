7489544c7e8dd413432ffee2b778771e1bc79e4b
racollette <racollette@gmail.com>
2023-04-27 10:21:39 -0400

    styling fix

--- src/views/Bridge/Bridge.tsx
+++ src/views/Bridge/Bridge.tsx
@@ -52,7 +52,7 @@ const BridgePage = styled(Page)`
     background: radial-gradient(40% 55% at 45% 57.5%, #f2ad6c 0%, rgba(242, 173, 108, 0.4) 25%, rgba(6, 9, 20, 0) 72.5%),
       radial-gradient(40% 45% at 55% 47.5%, #48b9ff 0%, rgba(72, 185, 255, 0.4) 25%, rgba(6, 9, 20, 0) 72.5%);
     filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
-    background-position-y: -2vh;
+    background-position-y: -10vh;
   }
 `
 
